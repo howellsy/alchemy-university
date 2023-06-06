8c8c411b8c8581adf2628bc4814b5d618ff23dd5
howellsy <jamie.howells@gmail.com>
2023-06-06 15:27:24 +0100

    Week 3 - Wallets exercise

---
+++ week-3/ethers-intro/wallets.spec.js
@@ -0,0 +1,24 @@
+const { assert } = require('chai');
+const { wallet1, wallet2 } = require('./wallets');
+const { HDNodeWallet, Wallet } = require('ethers');
+
+describe('wallets', () => {
+  describe('wallet 1', () => {
+    it('should be an instance of Wallet', () => {
+      assert(wallet1 instanceof Wallet);
+    });
+
+    it('should unlock the expected address', () => {
+      assert.equal(wallet1.address, '0x5409ED021D9299bf6814279A6A1411A7e866A631');
+    });
+  });
+  describe('wallet 2', () => {
+    it('should be an instance of Wallet', () => {
+      assert(wallet2 instanceof Wallet);
+    });
+
+    it('should unlock the expected address', () => {
+      assert.equal(wallet2.address, '0x88E9DD325BA8329dDD9825c1d24e8470b25575C1');
+    });
+  });
+});
