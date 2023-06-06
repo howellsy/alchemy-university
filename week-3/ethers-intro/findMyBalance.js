82cdd4b3e166e96739cd530bd5d7495ee0d60be3
howellsy <jamie.howells@gmail.com>
2023-06-06 15:30:01 +0100

    Week 3 - Find my balance

---
+++ week-3/ethers-intro/findMyBalance.js
@@ -0,0 +1,11 @@
+const { Wallet, providers } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+
+function findMyBalance(privateKey) {
+    const wallet = new Wallet(privateKey, provider);
+    return wallet.getBalance();
+}
+
+module.exports = findMyBalance;
